32383a02b8fec93a17654a0390815ec3435dd079
unknown <cuitlahuacjr@gmail.com>
2022-03-16 16:34:18 -0600

    Deleted getCharactersRange() method. The range will include from 0x0020 - 0x007E always

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,7 @@ function password(length, isLowerCase, isUpperCase, isNumeric, isSpecialChars){
   this.isNumeric = isNumeric;
   this.isSpecialChars = isSpecialChars;
   this.isValid = false;
-  this.minRange = 0;
-  this.maxRange = 0;
+  this.value = "";
 
   this.validateCriteria = function (){
     let isCriteriaOk = true;
@@ -24,21 +23,14 @@ function password(length, isLowerCase, isUpperCase, isNumeric, isSpecialChars){
     this.isValid = isCriteriaOk;
   };
 
-  this.getCharactersRange = function (){
-    if(this.isNumeric){
-      this.minRange = 0x0030;
-      this.maxRange = 0x0039;
-    }
-  };
+
 
   this.generatePassword = function(){
     let result = "";
 
-    this.getCharactersRange();
-
     for (let i = 0; i < this.length; i++) {
       // console.log(String.fromCharCode(this.minRange + Math.random() * (this.maxRange - this.minRange + 1)));
-      let character = String.fromCharCode(this.minRange + Math.random() * (this.maxRange - this.minRange + 1))
+      let character = String.fromCharCode(0x0020 + Math.random() * (0x007E - 0x0020 + 1))
       result += character;
     }
 
@@ -60,7 +52,7 @@ function writePassword() {
   pswd.validateCriteria();
   if(pswd.isValid){
     pswd.generatePassword();
-    var passwordText = document.querySelector("#password");
+    let passwordText = document.querySelector("#password");
     passwordText.value = pswd.value;
   }
   else {
@@ -77,18 +69,22 @@ function getPasswordLength() {
     return passwordLength;
 }
 
+// Asks user if password will contain lower case letters.
 function includeLowerCase(){
   return window.confirm("Include lower case letters in password?");
 }
 
+// Asks user if password will contain upper case letters.
 function includeUpperCase(){
   return window.confirm("Include upper case letters in password?");
 }
 
+// Asks user if password will contain numbers.
 function includeNumeric(){
   return window.confirm("Include numbers in password?");
 }
 
+// Asks user if password will contain special characters.
 function includeSpecialChars(){
   return window.confirm("Include special characters in password?");
 }
