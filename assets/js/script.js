e911804ce46fd13de57ef17a3910e46c9b0808fe
unknown <cuitlahuacjr@gmail.com>
2022-03-15 19:44:18 -0600

    Defined functions to get the password's criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,20 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var pswdLength = getPasswordLength();
+  let pswdLength;
+  let isLowerCase, isUpperCase, isNumeric, isSpecialChars;
+  pswdLength = getPasswordLength();
+  isLowerCase = includeLowerCase();
+  isUpperCase = includeUpperCase();
+  isNumeric = includeNumeric();
+  isSpecialChars = includeSpecialChars();
+
+  console.log(pswdLength);
+  console.log(isLowerCase);
+  console.log(isUpperCase);
+  console.log(isNumeric);
+  console.log(isSpecialChars);
+
   // var password = generatePassword();
   // var passwordText = document.querySelector("#password");
   //
@@ -11,22 +24,45 @@ function writePassword() {
 
 }
 
+// function getPasswordLength() {
+//   let passwordLength;
+//
+//   while (true) {
+//     passwordLength = window.prompt("Please choose the password length between 8 and 128 characters:");
+//     if (passwordLength !== null && passwordLength !== "") {
+//       if (passwordLength >= 8 && passwordLength <= 128) {
+//         return passwordLength;
+//       } else
+//         window.alert("You must enter a valid length");
+//     } else {
+//       window.alert("Please enter valid value");
+//     }
+//   }
+//
+// }
+
 // Asks user for the desired password's length.
 function getPasswordLength() {
   let passwordLength;
 
-  while (true) {
     passwordLength = window.prompt("Please choose the password length between 8 and 128 characters:");
-    if (passwordLength !== null && passwordLength !== "") {
-      if (passwordLength >= 8 && passwordLength <= 128) {
-        return passwordLength;
-      } else
-        window.alert("You must enter a valid length");
-    } else {
-      window.alert("Please enter valid value");
-    }
-  }
+    return passwordLength;
+}
 
+function includeLowerCase(){
+  return window.confirm("Include lower case letters in password?");
+}
+
+function includeUpperCase(){
+  return window.confirm("Include upper case letters in password?");
+}
+
+function includeNumeric(){
+  return window.confirm("Include numbers in password?");
+}
+
+function includeSpecialChars(){
+  return window.confirm("Include special characters in password?");
 }
 
 // Add event listener to generate button
