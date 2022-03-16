884b2fd82598b28c84bbcdbca02c6780dcbcdc6d
unknown <cuitlahuacjr@gmail.com>
2022-03-15 19:27:55 -0600

    Added function getPasswordLength() for user to input the desired length

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,25 @@ function writePassword() {
 
 }
 
+// Asks user for the desired password's length.
+function getPasswordLength() {
+  let passwordLength;
+
+  while (true) {
+    passwordLength = window.prompt("Please choose the password length between 8 and 128 characters:");
+    if (passwordLength !== null && passwordLength !== "") {
+      if (passwordLength >= 8 && passwordLength <= 128) {
+        return passwordLength;
+      } else
+        window.alert("You must enter a valid length");
+    } else {
+      window.alert("Please enter valid value");
+    }
+  }
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var pswdLength = getPasswordLength();
